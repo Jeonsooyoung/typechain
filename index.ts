69d3228dd8285f1b6f94ebbc5370343bbcb4242b
jeonsooyoung <peace2332@naver.com>
2020-12-26 20:16:46 +0900

    first

---
+++ index.ts
@@ -0,0 +1,12 @@
+const name = "soo",
+age = 25,
+gender = "male"
+
+const sayHi = (name,age,gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name,age,gender)
+
+//모듈화
+export {}
